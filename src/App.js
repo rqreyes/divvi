c29abfd6029a9898aa7fb688b70eff9212710ec9
Randy Reyes <rqreyes@gmail.com>
2020-04-16 02:16:18 -0700

    removed theme context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import ThemeContextProvider from './contexts/ThemeContext';
 import ItemContextProvider from './contexts/ItemContext';
 import PersonContextProvider from './contexts/PersonContext';
 import Header from './components/organisms/Header';
@@ -26,48 +25,46 @@ function App() {
   const themeDisplay = theme ? '1' : '2';
 
   return (
-    <ThemeContextProvider>
-      <div className={`App theme-${themeDisplay}`}>
-        <ItemContextProvider>
-          <PersonContextProvider>
-            <section className='header'>
-              <Header updateTheme={updateTheme} />
-            </section>
-            <section className='receipt'>
-              <Element name='receiptAnchor' />
-              <div className='heading-icon'>
-                <FontAwesomeIcon icon={faUtensils} />
-              </div>
-              <ItemList />
-              <ItemForm />
-              <ItemTotal />
-            </section>
-            <section className='people'>
-              <Element name='peopleAnchor' />
-              <div className='heading-icon'>
-                <FontAwesomeIcon icon={faUserFriends} />
-              </div>
-              <h2>People</h2>
-              <PersonList />
-              <PersonForm />
-            </section>
-            <section className='amount-left'>
-              <div className='heading-icon'>
-                <FontAwesomeIcon icon={faCommentsDollar} />
-              </div>
-              <h2>Amount Left</h2>
-              <AmountLeft />
-            </section>
-            <section className='about'>
-              <div className='heading-icon'>
-                <FontAwesomeIcon icon={faUserAstronaut} />
-              </div>
-              <About />
-            </section>
-          </PersonContextProvider>
-        </ItemContextProvider>
-      </div>
-    </ThemeContextProvider>
+    <div className={`App theme-${themeDisplay}`}>
+      <ItemContextProvider>
+        <PersonContextProvider>
+          <section className='header'>
+            <Header updateTheme={updateTheme} />
+          </section>
+          <section className='receipt'>
+            <Element name='receiptAnchor' />
+            <div className='heading-icon'>
+              <FontAwesomeIcon icon={faUtensils} />
+            </div>
+            <ItemList />
+            <ItemForm />
+            <ItemTotal />
+          </section>
+          <section className='people'>
+            <Element name='peopleAnchor' />
+            <div className='heading-icon'>
+              <FontAwesomeIcon icon={faUserFriends} />
+            </div>
+            <h2>People</h2>
+            <PersonList />
+            <PersonForm />
+          </section>
+          <section className='amount-left'>
+            <div className='heading-icon'>
+              <FontAwesomeIcon icon={faCommentsDollar} />
+            </div>
+            <h2>Amount Left</h2>
+            <AmountLeft />
+          </section>
+          <section className='about'>
+            <div className='heading-icon'>
+              <FontAwesomeIcon icon={faUserAstronaut} />
+            </div>
+            <About />
+          </section>
+        </PersonContextProvider>
+      </ItemContextProvider>
+    </div>
   );
 }
 
